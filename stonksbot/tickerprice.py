3df733cac8759eec21ccec9721e03797d6a749d7
MerinoSheep <35940408+MerinoSheep@users.noreply.github.com>
2020-05-11 00:04:02 -0500

    $ currency added

--- stonksbot/tickerprice.py
+++ stonksbot/tickerprice.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import yfinance as yf
 import numpy as np
+currency={
+    'us_market':'$'
+}
 df = pd.read_json('generic.json')
 def given_name(name):
     
     print(df.loc[df['Ticker']=='B'])
   
 def given_ticker(ticker):
-    stock=yf.download(ticker,period='1m',interval='1m')
-
-    return round(float(stock.tail(1)['Open']),2)
+    market_info = yf.Ticker(ticker).info['market']
+    stock = yf.download(ticker,period='1m',interval='1m')
+    currency_symbol = currency.get(market_info,'')
+    stock_price  = round(float(stock.tail(1)['Open']),2)
+    #return market_info
+    return currency_symbol + str(stock_price)
    
 
 if __name__ == "__main__":
     pass
 
-
+#print(given_ticker('AMD'))
